2aa726f1024ad8238d8afb2a19357317cfd79ffc
AlaaEid-1 <alaaalaa2382924@gmail.com>
2026-07-27 00:46:36 +0300

    Fix home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,14 +7,14 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function __invoke(Request $request)
-    {
-        $posts = Post::query()
-            ->published()
-            ->with(['user', 'tags'])
-            ->latest()
-            ->paginate(2);
+ public function __invoke(Request $request)
+{
+    $posts = Post::query()
+        ->published()
+        ->with(['user', 'tags'])
+        ->latest()
+        ->paginate(2);
 
-        return view('home', compact('posts'));
-    }
+    return response()->json($posts); // بدل return view('home', compact('posts'));
+}
 }
