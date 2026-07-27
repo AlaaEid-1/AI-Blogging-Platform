366fd080fbd3987b436d1b00ba082fa98147b4a3
AlaaEid-1 <alaaalaa2382924@gmail.com>
2026-07-27 13:57:51 +0300

    Fix bootstrap file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: 'app/api/'
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->web([
             UpdateUserLastActivityTime::class,
         ]);
